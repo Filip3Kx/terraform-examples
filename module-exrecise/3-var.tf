73174d9340bceb022ac0e5185b9587edd3847b37
Filip3K <114138650+Filip3Kx@users.noreply.github.com>
2023-09-14 14:15:38 +0200

    Update 3-var.tf

--- module-exrecise/3-var.tf
+++ module-exrecise/3-var.tf
@@ -1,1 +1,39 @@
-#modules vars
+variable "bucket_info" {
+    type = map
+    default = {
+        name = "tf-state-bucket115"
+        prefix = "terraform/state"
+    }
+}
+
+
+variable "google_info" {
+  type = map
+  default = {
+    project = "caramel-compass-393820"
+    region = "europe-west1-b"
+    sa_key = "key.json"
+  }
+}
+
+
+variable "vm1_info" {
+    type = map
+    default = {
+        name = "http1-vm"
+        size = "f1-micro"
+        image = "ubuntu-os-cloud/ubuntu-2004-lts"
+        network = "http-network"
+    }
+}
+
+
+variable "vm2_info" {
+    type = map
+    default = {
+        name = "http2-vm"
+        size = "f1-micro"
+        image = "ubuntu-os-cloud/ubuntu-2004-lts"
+        network = "http-network"
+    }
+}
