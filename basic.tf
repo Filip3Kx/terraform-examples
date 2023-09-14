99140435f25a4a0fe6fac16e8d293df9bbc1877f
Filip3K <114138650+Filip3Kx@users.noreply.github.com>
2023-09-14 14:49:01 +0200

    Update basic.tf

--- basic.tf
+++ basic.tf
@@ -10,40 +10,38 @@ provider "google" {
     project     = "caramel-compass-393820"
     region      = "europe-west1-b"
 }
-resource "google_compute_instance" "vm_world" {
-    name         = "vm-world"
-    machine_type = "f1-micro"
-    zone         = "europe-west1-b"
 
-    boot_disk {
-        initialize_params {
-            image = "ubuntu-os-cloud/ubuntu-2004-lts"
-        }
-    }
 
-    network_interface {
-        network = "default"
-    }
-
-    metadata_startup_script = <<-EOF
-        #!/bin/bash
-        #DOCKER INSTALL
-        sudo apt-get update
-        sudo apt-get install ca-certificates curl gnupg
-        sudo install -m 0755 -d /etc/apt/keyrings
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-        sudo chmod a+r /etc/apt/keyrings/docker.gpg
-        echo \
-        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-        sudo apt-get update
-        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose docker-buildx-plugin docker-compose-plugin -y
-    EOF
+resource "google_compute_firewall" "allow-http" {
+  name        = "allow-http"
+  network     = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
 }
 
-resource "google_app_engine_application" "app" {
-  project     = "caramel-compass-393820"
-  location_id = "europe-west1-b"
+
+resource "google_compute_instance" "http1-vm" {
+    name         = "http1-vm"
+    machine_type = "f1-micro"
+    zone         = "europe-west1-b"
+    tags         = ["allow-http"]
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-11-bullseye-v20230912"
+        }
+    }
+    network_interface {
+        network = "default"
+        access_config {
+        }
+    }
+    metadata_startup_script = <<-EOF
+        sudo apt update
+        sudo apt install -y nginx
+        echo "hello world 1" | sudo tee /var/www/html/index.html
+        sudo systemctl restart nginx
+    EOF
 }
